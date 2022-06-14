fd18148d5ab325fd410ec07b0c58bfc82553d170
SvetlanaGIP <gip1@acn.su>
2022-06-14 13:56:50 +1000

    Добавьте файлы проекта.

---
+++ HW17/Account.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW17
+{
+    class Account<T>
+    {
+        private T number;
+        private string fio;
+        private double balance;
+
+        public string GetInfo()
+        {
+            return $"{number} {fio} {balance}";
+        }
+
+        public void SetInfo()
+        {
+            Console.WriteLine("Введите номер");
+            string stringnumber = Console.ReadLine();
+            number = (T)Convert.ChangeType(stringnumber, typeof(T));
+
+            Console.WriteLine("Введите ФИО");
+            fio = Console.ReadLine();
+            Console.WriteLine("Введите баланс");
+            balance = Convert.ToInt32(Console.ReadLine());
+        }
+
+        /*public void Input()
+        {
+            Console.WriteLine("Введите номер");
+            string stringNum = Console.ReadLine();
+            Num = (T)Convert.ChangeType(stringNum, typeof(T));
+
+            Console.WriteLine("Введите имя");
+            Name = Console.ReadLine();
+            Console.WriteLine("Введите зп");
+            Summa = Convert.ToInt32(Console.ReadLine());
+        }*/
+    }
+}
---
+++ HW17/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW17
+{
+    class Program
+    {
+        //Создать класс для моделирования счета в банке.
+        //Предусмотреть закрытые поля для номера счета, баланса, ФИО владельца.
+        //Класс должен быть объявлен как обобщенный.
+        //Универсальный параметр T должен определять тип номера счета.
+        //Разработать методы  для доступа  к данным  –  заполнения и  чтения.
+        //Создать несколько экземпляров класса, параметризированного различными типам.
+        //Заполнить его поля и вывести информацию об экземпляре класса на печать.
+        static void Main(string[] args)
+        {
+            Account<String> account1 = new Account<string>();
+            account1.SetInfo();
+            Console.WriteLine(account1.GetInfo());
+
+            Account<String> account2 = new Account<string>();
+            account1.SetInfo();
+            Console.WriteLine(account2.GetInfo());
+
+            Console.ReadKey();
+        }
+    }
+}
